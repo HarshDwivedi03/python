3bc9e8d668054a2cc0ff4b87bd4f757849cc83bd
harsh <hdwivedi791@gmail>
2025-02-03 14:34:31 +0530

    while

---
+++ python_new/while.py
@@ -0,0 +1,14 @@
+# n=int(input("enter any no"))
+# i=1
+# while i<=n:
+#     print(i)
+#     i=i+1
+    n=int(input("enter any no"))
+    i=1
+    while(i<=n):
+        x=2*1
+        print(f'2*1{i}={x}')
+        print(x)
+        i+=1
+    
+    
