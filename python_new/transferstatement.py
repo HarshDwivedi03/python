2afa1039605077560da1a26c7c6326084e111808
harsh <hdwivedi791@gmail>
2025-02-06 14:32:03 +0530

    transferstatement

---
+++ python_new/transferstatement.py
@@ -0,0 +1,35 @@
+# # # # # trasferstatement
+# # # # # # break=exit from loop
+# # # # # continue = skip current iteration
+# # # # # pass =skip current block
+# # # # n=int(input("enter any no"))
+# # # # i=1
+# # # # while(i<=n):
+# # # #     if i==6:
+# # # #        break
+# # # #     print(i)
+# # # #     i=i+1
+# # # n=int(input("enter any no"))
+# # # i=1
+# # # while(i<=n):
+# # #     if i==6:
+# # #        i=i+1
+# # #        continue
+# # #     print(i)
+# # #     i=i+1
+# # n=int(input("enter any no"))
+# # i=1
+# # while(i<=n):
+# #     if i==6:
+# #        pass
+# #     print(i)
+# #     i=i+1
+# x=eval(input("enter any value"))
+# print(x)
+# print(type(x))
+# pattern
+n=int("enter many row required")
+i=1
+while(i<=n):
+    print(i*'*')
+    i=i+1
