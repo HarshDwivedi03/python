01533652b2679c3f243d07b8df661084a6035795
harsh <hdwivedi791@gmail>
2025-02-07 14:29:09 +0530

    pattern

---
+++ python_new/pattern.py
@@ -0,0 +1,27 @@
+# # # # pattern
+# # n=input("enter any string")
+# # n=5
+# # i=1
+# # while(i<=5):
+# #     print('*'*i)
+# #     i=i+1
+# # n=int(input("enter how many row required"))
+# # i=1
+# # while i<=n:
+# #     print(' '*(n-i)+' *'*i)
+# #     i=i+1
+# # n=int(input("enter how many row required"))
+# # i=n
+# # while n>=1:
+# #     print('*'*n)
+# #     n=n-1
+# n=int(input("enter how many row required"))
+# i=n
+# while i>=1:
+#     print(' '*(n-i)+' *'*i)
+#     i=i-1
+n=int(input("enter how many row required"))
+i=n
+while i>=1:
+    print(' '*(n-i)+' *'*i)
+    i=i-1
