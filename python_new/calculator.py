f7b088e05fc33b06ebd60bdefd4b064b1f3b14f1
harsh <hdwivedi791@gmail>
2025-02-08 14:29:36 +0530

    calculator and lcm

---
+++ python_new/calculator.py
@@ -0,0 +1,47 @@
+# # # calculator
+# # while True:
+# #     print("1.addition\n2.subtraction\n3.multiply\n4.division\n5.off")
+# #     n=int(input("enter your choice"))
+# #     if n==1:
+# #         x=int(input("enter first no:"))
+# #         y=int(input("enter second no:"))
+# #         z=x+y
+# #         print(f'addition of{x}and{y}is{z}')
+# #     elif n==2:
+# #         x=int(input("enter first no:"))
+# #         y=int(input("enter second no:"))
+# #         z=x-y
+# #         print(f'subtration of{x}and{y}is{z}')
+# #     elif n==3:
+# #         x=int(input("enter first no:"))
+# #         y=int(input("enter second no:"))
+# #         z=x*y
+# #         print(f'multiply of{x}and{y}is{z}')
+# #     elif n==4:
+# #         x=int(input("enter first no:"))
+# #         y=int(input("enter second no:"))
+# #         z=x/y
+# #         print(f'division of{x}and{y}is{z}')
+# #     elif n==5:
+# #         break
+# #     else:
+# #         print("please enter valid option")
+# while True:
+#     print("1.addition\n2.subtraction\n3.multiply\n4.division\n5.off")
+#     n=int(input("enter your choice"))
+#     p={1,2,3,4,5}
+x=int(input("enter your first choice"))
+x=int(input("enter your second choice"))
+max-no=max(x,y)
+while true:
+if max-no%x==0 and max-no%y==0:
+break
+max-no=max-no+1
+print(f'lcm of{x}and {y}is{max-no}')
+
+ 
+      
+
+
+            
+    
