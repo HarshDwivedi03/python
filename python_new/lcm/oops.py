4763bc530b7a5755e58a806b4016a8a689bc72d7
harsh <hdwivedi791@gmail>
2025-02-26 14:30:47 +0530

    oops

---
+++ python_new/lcm/oops.py
@@ -0,0 +1,9 @@
+# oops
+# class=bleprint of an class
+# properties =variables
+# action/behaviour=methods
+# object=intance of class
+# abtraction=data protection
+# encapsulation=data protection
+# inheritance=reusablity
+# polymorphism=reusablity
